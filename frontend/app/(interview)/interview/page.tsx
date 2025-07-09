282357f59c67896e40b75a933596cc2a65691257
LimeIvy <raimu10311221@yahoo.co.jp>
2025-07-10 01:47:05 +0900

    面接のレイアウト変更

--- frontend/app/(interview)/interview/page.tsx
+++ frontend/app/(interview)/interview/page.tsx
@@ -3,7 +3,7 @@ import InterviewList from './_components/InterviewList'
 
 const Interview = () => {
   return (
-    <div className="p-10">
+    <div className="p-10 mt-10">
       <h1 className="font-bold text-2xl">AI面接</h1>
       <h2 className="text-gray-500 mt-1">就活の面接練習をAIでサポート</h2>
 
