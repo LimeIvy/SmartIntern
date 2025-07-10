4a2ea82f7a7ec68b52b41638548270a56f22b6c7
Lime_Ivy <raimu10311221@yahoo.co.jp>
2025-07-10 19:38:42 +0900

    日程と名前の変更

--- frontend/lib/checkUser.ts
+++ frontend/lib/checkUser.ts
@@ -34,7 +34,7 @@ export const checkUser = async () => {
     const newUser = await db?.user.create({
       data: {
         clerkUserId: user.id,
-        name: user.username || "",
+        name: user.username || user.fullName,
         imageUrl: user.imageUrl,
         email: user.emailAddresses[0]?.emailAddress || "",
       },
